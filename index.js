ff78bc2dd01198a61c6b32d0a8e5ac09126be749
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-28 15:41:28 -0300

    Iniciado quest de Authentication level 1

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public", {
+  setHeaders: (res, path) =>{
+      if(path.endsWith(".css")){
+          res.set("Content-Type", "text/css")
+      }
+  }
+}))
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+
+
+app.post("/register", async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  console.log(username, password)
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  console.log(username, password)
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
