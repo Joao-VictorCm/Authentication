6087aa7d63b2b3bf6fa07f2a36dc93c579f6435f
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-28 16:20:20 -0300

    Adicionado verificação se o email ja foi cadastrado

--- index.js
+++ index.js
@@ -43,13 +43,22 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const email = req.body.username
   const password = req.body.password
-  const result = await db.query(
-    "INSERT INTO users (email, password) VALUES ($1, $2);",
-    [email, password]
+
+  const checkResult = await db.query("SELECT * FROM users WHERE email = ($1)",
+    [email]
   )
-  console.log(result)
-  res.render("secrets.ejs")
-  
+
+  if(checkResult.rows.length > 0){
+    res.send("Email already exists. Try logging in.")
+  }else{
+    const result = await db.query(
+      "INSERT INTO users (email, password) VALUES ($1, $2);",
+      [email, password]
+    )
+    console.log(result)
+    res.render("secrets.ejs")
+    
+  }
 });
 
 app.post("/login", async (req, res) => {
