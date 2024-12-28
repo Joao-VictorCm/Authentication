4d90fb2b77dc00e9dbc25324595f3373fea3cfc4
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-28 16:16:20 -0300

    Conectando o projeto com o banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg"
 
 const app = express();
 const port = 3001;
@@ -12,6 +13,18 @@ app.use(express.static("public", {
       }
   }
 }))
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "12345",
+  port: "5432"
+})
+
+db.connect()
+
+
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -28,15 +41,21 @@ app.get("/register", (req, res) => {
 
 
 app.post("/register", async (req, res) => {
-  const username = req.body.username
+  const email = req.body.username
   const password = req.body.password
-  console.log(username, password)
+  const result = await db.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2);",
+    [email, password]
+  )
+  console.log(result)
+  res.render("secrets.ejs")
+  
 });
 
 app.post("/login", async (req, res) => {
-  const username = req.body.username
+  const email = req.body.username
   const password = req.body.password
-  console.log(username, password)
+ 
 });
 
 app.listen(port, () => {
